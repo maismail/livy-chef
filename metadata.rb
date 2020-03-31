9df8f3c35a5363fbafade2632556660322c6bd70
Jim Dowling <jdowling@sics.se>
2020-03-31 11:38:07 +0200

    [HOPSWORKS-1679] Add (#37)
    * pin version of java in Berksfile
    
    * add Hopsworks cookbooks as depends in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,13 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.0"
 source_url       "https://github.com/hopshadoop/livy-chef"
 
-depends          "java", '~> 7.0.0'
+
+depends "java", '~> 7.0.0'
+depends 'kagent'
+depends 'hops'
+depends 'ndb'
+depends 'hadoop_spark'
+depends 'conda'
 
 recipe           "install", "Installs a Livy Spark REST Server"
 recipe           "default", "Starts  a Livy Spark REST Server"
